97c2488199ba497b62b63c70888383c0fd058fba
Sathish Kratos <rsathishikumar3@gmail.com>
2021-12-01 00:59:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,10 +19,17 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+app.get('/api', (req,res)=>{
+  var date = new Date();
+  res.send({unix: date.valueOf(), utc: date.toUTCString()});
+
+});
+
 
 app.get('/api/:date', (req,res)=>{
   var date = new Date(req.params.date);
